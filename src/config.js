94f60788687531a5156826487bffd5052df578a9
Ayusha Upreti <ayusha_upreti@hotmail.com>
2020-05-07 17:05:39 -0400

    replaced aws config to new resources

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-tutorial-uploading-bucket",
+    BUCKET: "uploads--bucket-notes-app",
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://s94cwbwkg0.execute-api.us-east-1.amazonaws.com/prod",
+    URL: "https://8g2koyc0q0.execute-api.us-east-1.amazonaws.com/prod",
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_6Djvj5Tq9",
-    APP_CLIENT_ID: "5jnhtp1a57mj337tpsrojicqpu",
-    IDENTITY_POOL_ID: "us-east-1:9465fe33-4eae-4b86-bb44-716c534097cd",
+    USER_POOL_ID: "us-east-1_mBd1jgg03",
+    APP_CLIENT_ID: "5b9grv16kijffh86lnvul1kt4l",
+    IDENTITY_POOL_ID: "us-east-1:bdc26a1c-f1d4-40e5-a020-53e2b95e14ef",
   },
   STRIPE_KEY: "pk_test_axeL169Nb9skE466nxPh5tUy00LduCuMZ7",
 };
