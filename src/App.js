c4fa2212406241c5c3458e0db79dcf04c2a6e6b2
Ayusha Upreti <ayusha_upreti@hotmail.com>
2020-05-10 09:41:48 -0400

    isolated problem to navbar in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Navbar, Nav, NavItem } from "react-bootstrap";
+// import { Navbar, Nav, NavItem } from "react-bootstrap";
+// import { Navbar, Nav, NavItem } from "reactstrap";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { onError } from "./libs/errorLib";
 import "./App.css";
@@ -13,6 +28,9 @@ function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const history = useHistory();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   useEffect(() => {
     onLoad();
@@ -40,32 +58,69 @@ function App() {
 
   return (
     !isAuthenticating && (
+      // <div className="App container">
+      //   <Navbar fluid collapseOnSelect>
+      //     <Navbar.Header>
+      //       <Navbar.Brand>
+      //         <Link to="/">Scratch</Link>
+      //       </Navbar.Brand>
+      //       <Navbar.Toggle />
+      //     </Navbar.Header>
+      //     <Navbar.Collapse>
+      //       <Nav pullRight>
+      //         {isAuthenticated ? (
+      //           <>
+      //             <NavItem onClick={handleLogout}>Logout</NavItem>
+      //           </>
+      //         ) : (
+      //           <>
+      //             <LinkContainer to="/login">
+      //               <NavItem>Login</NavItem>
+      //             </LinkContainer>
+      //             <LinkContainer to="/signup">
+      //               <NavItem>Signup</NavItem>
+      //             </LinkContainer>
+      //           </>
+      //         )}
+      //       </Nav>
+      //     </Navbar.Collapse>
+      //   </Navbar>
+      //   <ErrorBoundary>
+      //     <AppContext.Provider
+      //       value={{ isAuthenticated, userHasAuthenticated }}
+      //     >
+      //       <Routes />
+      //     </AppContext.Provider>
+      //   </ErrorBoundary>
+      // </div>
       <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              {isAuthenticated ? (
-                <>
-                  <NavItem onClick={handleLogout}>Logout</NavItem>
-                </>
-              ) : (
-                <>
-                  <LinkContainer to="/login">
-                    <NavItem>Login</NavItem>
-                  </LinkContainer>
-                  <LinkContainer to="/signup">
-                    <NavItem>Signup</NavItem>
-                  </LinkContainer>
-                </>
-              )}
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </Nav>
-          </Navbar.Collapse>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
         </Navbar>
         <ErrorBoundary>
           <AppContext.Provider
